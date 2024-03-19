c3def618a28473c0e08aa8a97a9e9638ebd75de7
Tansel Bayraktaroglu <tbayrakt@z3t9c2.42bangkok.com>
2024-03-19 15:24:11 +0700

    add va_list

--- ft_printf.c
+++ ft_printf.c
@@ -6,19 +6,25 @@
 /*   By: tbayrakt <tbayrakt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 15:29:08 by tbayrakt          #+#    #+#             */
-/*   Updated: 2024/03/16 15:33:40 by tbayrakt         ###   ########.fr       */
+/*   Updated: 2024/03/19 15:17:59 by tbayrakt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdarg.h>
 #include <stdio.h>
 
 int	ft_printf(const char *format, ...)
 {
+	va_list	args;
+
+	va_start(args, format);
+	va_end(args); // It will free the allocated memory
 	printf("Printing: %s\n", format);
 }
 
 int	main(void)
 {
 	char world[] = "Hello World!";
-	ft_printf(world);
+	int returnPrintf = ft_printf(world);
+	printf("Return value: %d\n", returnPrintf);
 }
