c779855afc6945ad18d49c16cc4302968b1fd899
Tansel Bayraktaroglu <tbayrakt@z3t2c3.42bangkok.com>
2024-03-21 12:28:46 +0700

    make it first work only with strings

--- ft_printf.c
+++ ft_printf.c
@@ -6,20 +6,31 @@
 /*   By: tbayrakt <tbayrakt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 15:29:08 by tbayrakt          #+#    #+#             */
-/*   Updated: 2024/03/19 15:17:59 by tbayrakt         ###   ########.fr       */
+/*   Updated: 2024/03/21 12:22:15 by tbayrakt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
+	int		i;
+	int		outputLength;
 
+	i = 0;
+	outputLength = 0;
 	va_start(args, format);
+	while (format[i])
+	{
+		outputLength += write(1, &format[i], 1);
+		i++;
+	}
 	va_end(args); // It will free the allocated memory
-	printf("Printing: %s\n", format);
+	return (outputLength);
+	// printf("Printing: %s\n", format);
 }
 
 int	main(void)
